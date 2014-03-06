bae8b3b3615fd778b39f5e8907608d0a9745926b
Viet-Trung Vu <trungvv91@gmail.com>
2014-03-06 14:48:46 +0700

    First Done

--- src/com/dsvn/ui/TokenizationDemo.java
+++ src/com/dsvn/ui/TokenizationDemo.java
@@ -130,6 +130,7 @@ public class TokenizationDemo extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new TokenizationDemo().setVisible(true);
             }
--- src/com/dsvn/util/IOUtil.java
+++ src/com/dsvn/util/IOUtil.java
@@ -5,14 +5,7 @@
  */
 package com.dsvn.util;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.logging.Level;
